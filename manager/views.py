bce7b898a3947e81d6af7e83060deed38604ec6f
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-04-30 21:13:36 +0600

    implement views, urls and templates in 'manager' app for authentication

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django import forms
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["name", "username", "email", "password1", "password2"]
+
+
+def registration_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
+    else:
+        form = RegistrationForm()
+
+    return render(request, "registration/register.html", {"form": form})
+
+
+def index(request):
+    return render(request, "index.html")
