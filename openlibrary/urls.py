f44c852f4214cd282768ad8ad304340b4940392b
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-04-30 21:30:38 +0600

    map urls for home, auth and catalogue

--- openlibrary/urls.py
+++ openlibrary/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for openlibrary project.
+URL configuration for locallibrary project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('~admin/', admin.site.urls),
+    path("home/", include("manager.urls")),
+    path('', lambda request: redirect('/home')),
+    path("auth/", include("django.contrib.auth.urls")),
+    path("catalogue/", include("catalogue.urls")),
 ]
