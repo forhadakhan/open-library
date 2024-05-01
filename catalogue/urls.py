3c810553706732a3cd17c7eb5b3cc6b8dd918e96
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-05-02 02:14:57 +0600

    feat: shows all books

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.catalogue, name="catalogue"),
+    path('book/all/', views.all_books, name='all_books'),
     path('book/add/', views.add_book, name='add_book'),
 ]
 
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .forms import *
 
@@ -20,3 +21,14 @@ def add_book(request):
     else:
         form = BookForm()
     return render(request, 'catalogue/book_add.html', {'form': form})
+
+
+def all_books(request):
+    all_books_list = Book.objects.all()
+    paginator = Paginator(all_books_list, 30)  # Show 30 books per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'catalogue/books.html', {'page_obj': page_obj})
+
