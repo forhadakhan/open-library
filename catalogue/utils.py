04c0163325bd9b2b84fa9b922bbe2151670a9444
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-05-04 02:06:49 +0600

    feat: show all manage members for admin

--- catalogue/utils.py
+++ catalogue/utils.py
@@ -3,6 +3,6 @@ dashboard_menu_options = [
     {"title": "All Books", "url_name": "all_books", "icon": "bi bi-book"},
     {"title": "Add Book", "url_name": "add_book", "icon": "bi bi-plus-square", "staff_only": True},
     {"title": "My Favorites", "url_name": "favorite_books", "icon": "bi bi-heart-half"},
-    # {"title": "Manage Members", "url_name": "manage_members", "icon": "bi bi-people", "staff_only": True}
+    {"title": "Members", "url_name": "user_list", "icon": "bi bi-people", "staff_only": True}
 ]
 
