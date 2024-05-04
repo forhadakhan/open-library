0be125620422ec48e7143c12b79df1043e198b2b
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-05-04 18:44:37 +0600

    func: define method to validate_rating

--- catalogue/utils.py
+++ catalogue/utils.py
@@ -1,8 +1,25 @@
-# Define menu options as a list of dictionaries
+# Define menu options for dashboard as a list of dictionaries
 dashboard_menu_options = [
     {"title": "All Books", "url_name": "all_books", "icon": "bi bi-book"},
     {"title": "Add Book", "url_name": "add_book", "icon": "bi bi-plus-square", "staff_only": True},
     {"title": "My Favorites", "url_name": "favorite_books", "icon": "bi bi-heart-half"},
-    {"title": "Members", "url_name": "user_list", "icon": "bi bi-people", "staff_only": True}
+    {"title": "Members", "url_name": "all_users", "icon": "bi bi-people", "staff_only": True}
 ]
 
+def validate_rating(rating):
+    """
+    Validates if the rating is within the range of 1 to 5.
+
+    Args:
+        rating (int): The rating to be validated.
+
+    Returns:
+        int: The validated rating.
+
+    Raises:
+        ValueError: If the rating is not within the range of 1 to 5.
+    """
+    rating = int(rating)
+    if rating < 1 or rating > 5:
+        raise ValueError("Rating must be between 1 and 5")
+    return rating
